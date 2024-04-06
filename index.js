0a9a3662f63feae17f02c45f2e3a9c1b6bd449b4
Hassan Qadri <qadrihassan@gmail.com>
2024-04-07 03:19:33 +0500

    nodejs project first commit

---
+++ index.js
@@ -0,0 +1,72 @@
+require('dotenv').config()
+
+const express = require("express")
+const app = express();
+
+const {todos_data} = require('./data')
+
+app.use(express.json())
+
+
+app.get("/task", (req, res) => {
+    const allData = {
+        data : todos_data,
+    }
+    return res.json( allData );
+});
+
+
+
+app.get('/task/:id' , (req,res) => {
+
+    const id = req.params.id
+
+    const find_todo = todos_data.filter((elem) => {
+        return elem.id == id
+    })
+
+    return res.json({
+        new_data : find_todo
+    })
+})
+
+
+app.post("/task", (req, res) => {
+
+    const new_data = req.body;
+
+    return res.json({
+      data: new_data,
+      message: "Successfully added!",
+    });
+  });
+
+
+
+app.put("/task/:id", (req, res) => {
+ 
+    const id = req.params.id;
+    
+
+    const data = req.body;
+  
+    const find_todo = todos_data.find((elem) => {
+        return elem.id == id;
+    });
+
+    if(!find_todo){
+          return res.status(404).json({ message: "Todo not found!" })
+    }
+    const update_todo = { ...find_todo, ...data };
+
+    return res.json({ data: update_todo });
+
+  });
+  
+
+console.log(process.env.PORT)
+
+
+app.listen(process.env.PORT, () => {
+    console.log("Express server is running!");
+ });
